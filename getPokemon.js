13ae69d65d07709186149cb003188deecb55e24f
asmitt8373 <austinsmittle8373@gmail.com>
2022-09-22 21:23:09 -0700

    pokedex

---
+++ getPokemon.js
@@ -0,0 +1,21 @@
+const { getPokemon, getAllPokemon, getAllPokemonNames } = require("pkmonjs");
+
+// const poke = getAllPokemon().then((pokemons) => {
+//   if (pokemons) {
+//     console.log(pokemons);
+//   }
+// });
+// const poke = getPokemon("squirtle").then((pokemon)=>
+// {
+//    if(pokemon) {
+//      console.log(pokemon.stats)
+//    }
+// }
+// )
+async function getAll(){
+    const names =await getAllPokemonNames()
+    console.log(names)
+    
+}
+
+getAll()
