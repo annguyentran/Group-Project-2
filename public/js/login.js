d3c645cbd054675d9a78a182a0c23194dd35b105
Enygma <miguelangelperezrefugio54@gmail.com>
2022-09-27 21:10:33 -0700

    new data

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,15 @@
-const trainer = async (event) => {
+const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const user = document.querySelector('#trainer').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-  if (user) {
+  if (email && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ user }),
+      body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
@@ -21,17 +22,17 @@ const trainer = async (event) => {
   }
 };
 
-
-
-const newTrainer = async (event) => {
+const signupFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
-  if (name) {
+  if (name && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name }),
+      body: JSON.stringify({ name, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
@@ -50,6 +51,5 @@ document
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
-
 
 
