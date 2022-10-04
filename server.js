37e6315d476b7e49582cf03ad84107f89e73ff46
Andrew Nguyen-Tran <ann013@ucsd.edu>
2022-10-03 18:06:18 -0700

    changed the ports

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ const sess = {
 
 app.use(session(sess));
 
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -44,5 +45,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(process.env.PORT || 3001, () => console.log('Now listening'));
 });
